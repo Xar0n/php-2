1676db59bf0b41e44cb1d2ba518dc9268d953e79
Tony <dima.dima.maksimov1998@mail.ru>
2017-11-11 12:00:32 +0800

    Add exceptions

---
+++ App/Exceptions/Http404Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 10.11.2017
+ * Time: 20:57
+ */
+
+namespace App\Exceptions;
+
+
+class Http404Exception extends HttpException
+{
+
+}
