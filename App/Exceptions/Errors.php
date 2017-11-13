04510e3eac48cde40eb914958406885a230353b6
Tony <dima.dima.maksimov1998@mail.ru>
2017-11-13 19:42:54 +0800

    Small changes

--- App/Exceptions/Errors.php
+++ App/Exceptions/Errors.php
@@ -3,8 +3,12 @@
 namespace App\Exceptions;
 
 
+use App\Traits\Iterator;
+
 class Errors extends \Exception
+	implements \Iterator
 {
+	use Iterator;
 	protected $data = array();
 
 	public function add(\Throwable $e)
